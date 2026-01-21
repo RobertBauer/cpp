6f7331d673cbc2c2f796bb38ed4f73c789319afa
Robert Bauer <rob.bauer@hotmail.com>
2026-01-21 20:15:54 -0300

    Add printNumbers function to output numbers 1 to 10

--- HelloWorld.cpp
+++ HelloWorld.cpp
@@ -1,6 +1,13 @@
 #include <iostream> 
 
+void printNumbers() {
+    for (int i = 1; i <= 10; i++) {
+        std::cout << i << std::endl;
+    }
+}
+
 int main() { 
     std::cout << "Hello, World!" << std::endl; 
+    printNumbers();
     return 0; 
 }
