0f3435f75530c2485918b9fdd7766ea50b3291fd
Robert Bauer <rob.bauer@hotmail.com>
2026-01-21 20:05:42 -0300

    Add function to print current system date in HelloWorld.cpp

--- HelloWorld.cpp
+++ HelloWorld.cpp
@@ -1,6 +1,20 @@
 #include <iostream> 
+#include <chrono>
+#include <ctime>
+#include <iomanip>
+
+void printSystemDate() {
+    auto now = std::chrono::system_clock::now();
+    auto time_t = std::chrono::system_clock::to_time_t(now);
+    std::tm* local_time = std::localtime(&time_t);
+    
+    std::cout << "Current date: " 
+              << std::put_time(local_time, "%Y-%m-%d") 
+              << std::endl;
+}
 
 int main() { 
     std::cout << "Hello, World!" << std::endl; 
+    printSystemDate();
     return 0; 
 }
